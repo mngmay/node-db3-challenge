3e70d088e8f1507000ab36586fc9967949ce487e
May Ng <mngmay@gmail.com>
2019-09-11 12:48:48 -0700

    Created findById

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,33 @@
 const db = require("../data/db-config.js");
 
 module.exports = {
-  find
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
 };
 
 function find() {
   return db("schemes");
 }
+
+function findById(id) {
+  return db("schemes")
+    .where({ id: id })
+    .then(scheme => {
+      return scheme;
+    })
+    .catch(err => {
+      return null;
+    });
+}
+
+function findSteps() {}
+
+function add() {}
+
+function update() {}
+
+function remove() {}
